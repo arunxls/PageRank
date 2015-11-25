2f7a908ef1823218dc53834494086dab04a6c621
Arun Krishnakumar <arunxls@gmail.com>
2015-11-24 21:47:25 -0600

    Removed templates

--- PageRank/include.h
+++ PageRank/include.h
@@ -11,62 +11,19 @@ typedef __int32 int32;
 typedef unsigned short   ushort;
 
 #pragma pack(push,1) // change struct packing to 1 byte
-template <typename T1, typename T2>
 class HeaderGraph {
 public:
-    T1 hash;
+    uint32 hash;
     uint32 len;
 
-    bool operator==(const HeaderGraph<T1, T2>& a) const
+    bool operator==(const HeaderGraph& a) const
     {
         return hash == a.hash;
     }
 
-    HeaderGraph<T1, T2>& operator+=(const HeaderGraph<T1, T2>& rhs)
-    {
-        if ((char*)this == (char*)&rhs) return *this;
-
-        uint32 tmp_len = rhs.len;
-        T2* dst = (T2*)(((char*) this) + this->size());
-        T2* src = (T2*)((&rhs) + 1);
-        memmove(dst, src, tmp_len*sizeof(T2));
-
-        this->len += tmp_len;
-        return *this;
-    }
-
     uint32 size()
     {
-        return sizeof(HeaderGraph<T1, T2>) + (sizeof(T2)*this->len);
-    }
-
-    HeaderGraph<T1, T2>& operator=(const HeaderGraph<T1, T2>& rhs)
-    {
-        if ((char*)this == (char*)&rhs) return *this;
-        hash = rhs.hash;
-        len = 0;
-
-        *this += rhs;
-
-        return *this;
-    }
-
-    bool operator<(const HeaderGraph<T1, T2>& rhs)
-    {
-        return hash < rhs.hash;
-    }
-};
-
-template <typename T>
-class RenamedHeaderGraph {
-public:
-    T old;
-    uint32 len;
-    uint32 renamed;
-
-    bool operator<(const RenamedHeaderGraph<T>& a) const
-    {
-        return old < a.old;
+        return sizeof(HeaderGraph) + (sizeof(uint32)*this->len);
     }
 };
 #pragma pack(pop)
